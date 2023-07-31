4e55d6141105466aeda7e73e215c76bbe4c0ac51
akshitasahu <akshitasah999@.com>
2023-07-31 08:27:57 +0530

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+var tl = gsap.timeline()
+
+tl.from("#page1 svg",{
+    y:-50,
+    opacity:0,
+    delay:0.3,
+    duration:0.5
+})
+.from("#page1 img",{
+    scale:0.5,
+    delay:-0.1,
+    duration:1,
+    // ease:Power4,
+    ease: Power4.easeOut
+})
+.from("#nav",{
+    y:-50,
+    opacity:0,
+    delay:-0.4,
+    duration:0.5
+})
